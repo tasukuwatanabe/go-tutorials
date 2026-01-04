78732a80d36221dba83041d6eeb4e8f053be93f4
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2026-01-04 15:37:50 +0900

    Add a test

---
+++ greetings/greetings_test.go
@@ -0,0 +1,22 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Tasuku"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Errorf(`Hello(%q) = %q, %v, want match for %#q, nil`, name, msg, nil, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Errorf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
